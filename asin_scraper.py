2b9c3e9f90ee6f49497bec6cf52ea9df603933d0
pwnzerer <hamzaidrees2008@gmail.com>
2022-11-18 15:15:57 +0500

    pogger

--- asin_scraper.py
+++ asin_scraper.py
@@ -5,15 +5,15 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 
 def get_asin(asin):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(f"https://www.amazon.com/dp/{asin}")
     html_sauce = driver.page_source
     driver.close()
